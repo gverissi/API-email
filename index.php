83f82d8d6a1523580e594f1d6303d3306739eb11
greg <greg.verissimo@gmail.com>
2020-06-18 09:33:21 +0200

    sould be ready for heroku

--- index.php
+++ index.php
@@ -4,24 +4,25 @@ use App\Controllers\MainController;
 
 require __DIR__ . '/vendor/autoload.php';
 
+// Load environnement data
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+// // Test purposes
+// $email = "greg@gmail.com";
+// $controller = new MainController();
+// $controller->addEmail($email);
 
-$email = "greg@gmail.com";
-$controller = new MainController();
-$controller->addEmail($email);
+header('Content-Type: application/json ; charset=utf-8');
+// header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
+header('Access-Control-Allow-Origin: *');
 
-// header('Content-Type: application/json ; charset=utf-8');
-// // header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
-// header('Access-Control-Allow-Origin: *');
+$req_method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'];
 
-// $req_method = $_SERVER['REQUEST_METHOD'];
-// $path = $_SERVER['PATH_INFO'];
-
-// if ($path === "/api/email/add" && $req_method === "POST") {
-// 	$data = json_decode(file_get_contents("php://input"), true);
-// 	$email = (string) $data["email"];
-// 	$controller = new MainController();
-//     $controller->addEmail($email);
-// }
+if ($path === "/api/email/add" && $req_method === "POST") {
+	$data = json_decode(file_get_contents("php://input"), true);
+	$email = (string) $data["email"];
+	$controller = new MainController();
+    $controller->addEmail($email);
+}
