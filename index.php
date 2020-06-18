7e1b8fcbbd4ad3270d3cc48774da3f2e6094171d
greg <greg.verissimo@gmail.com>
2020-06-18 11:12:52 +0200

    refacto index.php

--- index.php
+++ index.php
@@ -4,14 +4,9 @@ use App\Controllers\MainController;
 
 require __DIR__ . '/vendor/autoload.php';
 
-if (file_exists(__DIR__ . '/.env')) {
-    $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-}
-
-// // Load environnement data
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
+// Load environnement data
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 // // Test purposes
 // $email = "greg@gmail.com";
@@ -19,21 +14,13 @@ if (file_exists(__DIR__ . '/.env')) {
 // $controller->addEmail($email);
 
 header('Content-Type: application/json ; charset=utf-8');
-// header('Access-Control-Allow-Origin: http://127.0.0.1:5500');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 $req_method = $_SERVER['REQUEST_METHOD'];
-// $path = $_SERVER['PATH_INFO'];
-$path = $_GET["route"];
+$route = $_GET["route"];
 
-// var_dump($req_method);
-// var_dump($path);
-
-// echo $req_method;
-// echo $_GET["route"];
-
-if ($path === "email" && $req_method === "POST") {
+if ($route === "email" && $req_method === "POST") {
 	$data = json_decode(file_get_contents("php://input"), true);
 	$email = (string) $data["email"];
 	$controller = new MainController();
